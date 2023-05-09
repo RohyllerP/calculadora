56d13ac72fc089c4b88e57f613a60bc8c112a948
Rohyller Pereira <rohyler@gmail.com>
2023-05-09 08:54:32 -0400

    cambiarSwitch

--- assets/js/switch.js
+++ assets/js/switch.js
@@ -5,22 +5,40 @@ const hTwo = document.getElementById('calcu-h4');
 const tableDiv = document.getElementsByClassName('table-div');
 const modoSpan = document.getElementById('modo-span');
 
-radio.onclick = function(){
-    if(radio.checked){
+
+if (localStorage.getItem("checked") != undefined) {
+    radio.setAttribute("checked", true);
+    body.classList.add('body-dark');
+    h.classList.remove('h4-dark');
+    h.classList.add('color-light');
+    hTwo.classList.add('color-light');
+    hTwo.classList.remove('h4-dark');
+    body.classList.remove('body-light', 'color-dark');
+    modoSpan.innerHTML = "Modo oscuro";
+    modoSpan.style.color = "#FFFFFF";
+}
+
+radio.onclick = validar;
+function validar() {
+    if (radio.checked) {
+        radio.setAttribute("checked", true);
         body.classList.add('body-dark');
         h.classList.remove('h4-dark');
         h.classList.add('color-light');
         hTwo.classList.add('color-light');
         hTwo.classList.remove('h4-dark');
-        body.classList.remove('body-light','color-dark');
+        body.classList.remove('body-light', 'color-dark');
         modoSpan.innerHTML = "Modo oscuro";
         modoSpan.style.color = "#FFFFFF";
-    }else{
+        localStorage.setItem("checked", "true");
+    } else {
         body.classList.remove('body-dark');
-        body.classList.add('body-light','color-dark');
+        body.classList.add('body-light', 'color-dark');
         h.classList.add('h4-dark');
         hTwo.classList.add('h4-dark');
         modoSpan.innerHTML = "Modo claro";
         modoSpan.style.color = "#3E3E3E";
+        radio.setAttribute("checked", false);
+        localStorage.removeItem("checked");
     }
 }
